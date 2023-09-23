5af57a1f1c014afde54e1d61e2c0f165f56c253b
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-09-23 11:26:49 +0300

    quiz page added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,18 @@
-import { useState, useMemo, createContext } from 'react';
-import { AddWordForm } from './AddWordForm/AddWordForm';
-import { Container } from '@mui/material';
-import { WordsList } from './WordsList/WordsList';
-import Filter from './Filter/Filter';
-import { useLocalStorage } from 'hooks/useLocalStorage';
+import Home from 'pages/Home';
+import { Quiz } from 'pages/Quiz';
+import { createContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout';
 
 export const WordsContext = createContext(null);
 
 export const App = () => {
-  const [words, setWords] = useLocalStorage('words', []);
-  const [filter, setFilter] = useState('');
-
-  const addWord = newWord => {
-    setWords([...words, newWord]);
-  };
-
-  const handleDeleteWord = id => {
-    setWords(words.filter(word => word.id !== id));
-  };
-
-  const handleChange = evt => {
-    setFilter(evt.target.value);
-  };
-
-  const getFilterWords = useMemo(() => {
-    const normalizedFilter = filter.toLowerCase().trim();
-    return words.filter(word => {
-      return word.ukWord
-        .concat(word.enWord)
-        .toLowerCase()
-        .includes(normalizedFilter);
-    });
-  }, [filter, words]);
-
-  const handelEditWord = editedWord => {
-    setWords(prevState => {
-      return prevState.map(word =>
-        word.id === editedWord.id ? editedWord : word
-      );
-    });
-  };
-
   return (
-    <Container maxWidth="xl">
-      <WordsContext.Provider value={{ handelEditWord }}>
-        <AddWordForm addNewWord={addWord} />
-        <Filter value={filter} handleChange={handleChange} />
-        <WordsList words={getFilterWords} onDeleteWord={handleDeleteWord} />
-      </WordsContext.Provider>
-    </Container>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="quiz" element={<Quiz />} />
+      </Route>
+    </Routes>
   );
 };
